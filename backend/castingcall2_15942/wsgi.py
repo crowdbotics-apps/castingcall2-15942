22e22c1814c584226d0c93d621c19839a72468f1
Crowdbotics <team@crowdbotics.com>
2020-04-17 01:18:38 +0000

    Update models

--- backend/castingcall2_15942/wsgi.py
+++ backend/castingcall2_15942/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'castingcall2_15942.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "castingcall2_15942.settings")
 
 application = get_wsgi_application()
